4cbad9e8d8d7029e197fa86586a0f2a6b9d8a3d5
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-12-27 00:00:55 +0000

    Begin __init__.py

---
+++ tests/acceptance/__init__.py
@@ -0,0 +1,8 @@
+"""Acceptance tests for Highway Driver SDK.
+
+These tests run against the production Highway API.
+Requires HIGHWAY_API_KEY environment variable.
+
+Run with:
+    HIGHWAY_API_KEY=... pytest tests/acceptance/ -v
+"""
