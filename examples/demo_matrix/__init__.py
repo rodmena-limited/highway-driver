1cafa45db91f6d68b74e992c6b7b74529c9d631c
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-12-29 01:00:39 +0000

    Architect __init__ Python code

---
+++ examples/demo_matrix/__init__.py
@@ -0,0 +1,5 @@
+"""Demo Matrix - Systematic corner case testing for Highway Driver SDK.
+
+Port of highway-workflow-engine/scripts/demo/demo_matrix/ to driver SDK.
+Tests run against production highway.solutions.
+"""
