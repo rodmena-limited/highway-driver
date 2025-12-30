bed6dc94e2497713c1b196a8a87f726aa11209a6
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-12-30 19:00:37 +0000

    Add __init__.py

---
+++ examples/test_package/__init__.py
@@ -0,0 +1,9 @@
+"""Test package for acceptance testing of tools.python.run support.
+
+This package demonstrates a proper Python package structure with:
+- Multiple modules that import from each other
+- A main entrypoint function that receives DurableContext
+- Cross-module dependencies
+"""
+
+__version__ = "1.0.0"
